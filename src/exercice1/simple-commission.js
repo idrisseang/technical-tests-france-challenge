79835220aee89b786b7b0ba30ed431fa09803e45
jorisgrls <jorisgrilleres@gmail.com>
2024-05-13 16:07:50 +0200

    feat : add exercises and tests

---
+++ src/exercice1/simple-commission.js
@@ -0,0 +1,12 @@
+const PRIME_SUR_ENGAGEMENT = 2.5 / 100;
+const MIN_ENGAGEMENT = 70 / 100;
+
+// ca : int (représente le chiffre d'affaire d'une classe)
+// exemple : 1200
+
+// eleves : tableau d'objets (représente les élèves de la classe)
+// exemple : [{name: "Jonh", id: 1, hasSold: true}, {name: "Jack", id: 2, hasSold: false}]
+
+export const getCommission = (ca, eleves) => {
+  return [];
+};
