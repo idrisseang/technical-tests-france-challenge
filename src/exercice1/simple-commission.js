9e84adae8d86c35c92b471ff28ff60ae2036d5de
jorisgrls <jorisgrilleres@gmail.com>
2024-05-14 09:19:12 +0200

    feat : add example

--- src/exercice1/simple-commission.js
+++ src/exercice1/simple-commission.js
@@ -7,6 +7,10 @@ const MIN_ENGAGEMENT = 70 / 100;
 // eleves : tableau d'objets (représente les élèves de la classe)
 // exemple : [{name: "Jonh", id: 1, hasSold: true}, {name: "Jack", id: 2, hasSold: false}]
 
+// Sortie attendue : objet avec deux propriétés
+// - commission_taux : float (le taux de commission en %)
+// - commission_amount : int (le montant de la commission en €)
+
 export const getCommission = (ca, eleves) => {
   return [];
 };
